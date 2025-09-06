9013c7d66e65a1a9448b6dfa3a77cab57cdd3453
Nergis <nergis.melahat@gmail.com>
2025-09-06 21:46:54 +0300

    1.ders

---
+++ src/D02_DataCasting/A01_DataCasting.java
@@ -0,0 +1,59 @@
+package D02_DataCasting;
+
+public class A01_DataCasting {
+    public static void main(String[] args) {
+        int b=12;
+       // byte a=b;
+
+
+        byte a=(byte)b;
+
+
+        char harf='a';
+        int sayi=100;
+        System.out.println(harf+sayi);//197
+        System.out.println(harf+1);//98
+
+        char yeniHarf=(char) (harf+1);//
+        System.out.println(yeniHarf);//b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
