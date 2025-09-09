4ae4d73043d81ad2cf097f79e5ec626eb17c3e02
Nergis <nergis.melahat@gmail.com>
2025-09-09 21:40:28 +0300

    soru 1

---
+++ src/D02_DataCasting/A02_1/Soru.java
@@ -0,0 +1,83 @@
+package D02_DataCasting.A02_1;
+
+import java.util.Scanner;
+
+public class Soru {
+    public static void main(String[] args) {
+        //Soru 1- Int olarak verilen 3 degerin ortalamasini double olarak yazdiran bir kod
+        //yazin
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Lütfen Sayı1 Giriniz : ");
+        int sayı1=scanner.nextInt();
+        System.out.println("Lütfen Sayı2 Giriniz : ");
+        int sayı2=scanner.nextInt();
+        System.out.println("Lütfen Sayı3 Giriniz : ");
+        int sayı3=scanner.nextInt();
+
+
+        double sayıOrtalama=(sayı1+sayı2+sayı3)/3;
+        System.out.println("Verilen Üç Sayının Ortalaması:"+sayıOrtalama);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
