18c2995165fa3ad6c396d510fe4882857ab27baf
Nergis <nergis.melahat@gmail.com>
2025-09-12 22:13:27 +0300

    modulus soru 1

---
+++ src/B03_RaperVeMatematikselİşlemler/F01_RaperClass.java
@@ -0,0 +1,51 @@
+package B03_RaperVeMatematikselİşlemler;
+
+public class F01_RaperClass {
+    public static void main(String[] args) {
+        String nutellaFiyat1="23";
+        String nutellaFiyat2="45";
+
+        //String olarak verilen iki ürünün fiyatını toplayın....
+
+        System.out.println(nutellaFiyat1+nutellaFiyat2);
+
+
+        //İçinde SADECE sayılar olduğundan emin olduğunuz Stringleri
+        //Wrapper class'ları kullanarak sayıya çevirebiliriz....
+
+        int fiy1=Integer.parseInt(nutellaFiyat1);
+        int fiy2=Integer.parseInt(nutellaFiyat2);
+        System.out.println(fiy1+fiy2);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/B03_RaperVeMatematikselİşlemler/F02_ModulusSoru1.java
@@ -0,0 +1,67 @@
+package B03_RaperVeMatematikselİşlemler;
+
+import java.util.Scanner;
+
+public class F02_ModulusSoru1 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Lütfen 4 basamaklı bir sayı giriniz :");
+        int verilenSayı=scanner.nextInt();
+
+        int birlerBasamağı=verilenSayı%10;
+        int sayınınÜçBasamaklıHali=verilenSayı/10;
+        int onlarBasamağı=sayınınÜçBasamaklıHali/10;
+        int sayınınİkiBasamaklıHali=sayınınÜçBasamaklıHali/10;
+        int yüzlerBasamağı=sayınınİkiBasamaklıHali/10
+        int binlerBasamaklıHali=sayınınİkiBasamaklıHali/10;
+        System.out.println(birlerBasamağı+onlarBasamağı+yüzlerBasamağı+binlerBasamaklıHali);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
