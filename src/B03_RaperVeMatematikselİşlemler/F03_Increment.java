3d97ec757b6757174140b59cefffd04c2b8e6116
Nergis <nergis.melahat@gmail.com>
2025-10-18 21:36:18 +0300

    çalışma

---
+++ src/B03_RaperVeMatematikselİşlemler/F03_Increment.java
@@ -0,0 +1,57 @@
+package B03_RaperVeMatematikselİşlemler;
+
+public class F03_Increment {
+    public static void main(String[] args) {
+        /*
+        Toplama veya çarpma yaparak bir verejable 'in değerini artırabilirsiniz
+        Incre
+         */
+        int sayi=5;
+
+        sayi=sayi+5;
+
+        System.out.println("sayının 15.satırdaki degeri : "+ sayi);//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
