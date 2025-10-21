c36222ca43bb0da2b6baf214b2eaa85db4b9852b
Nergis <nergis.melahat@gmail.com>
2025-10-21 19:41:19 +0300

    ders4

---
+++ src/B03_RaperVeMatematikselİşlemler/F04_PrePostIncrement2.java
@@ -0,0 +1,47 @@
+package B03_RaperVeMatematikselİşlemler;
+
+public class F04_PrePostIncrement2 {
+    public static void main(String[] args) {
+
+        int a=10;
+        System.out.println("a 'nın değeri : "+ ++a); //11
+
+        int b=a++;
+        System.out.println("b'nin değeri : "+b);//b=11 a=12
+
+        int c=b++ +a;
+        System.out.println("c'nin değeri : "+c);//23 b=12
+
+        System.out.println("son toplam "+(a+b+c));//23+12+12=47
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/F04_PrePost.java
+++ src/F04_PrePost.java
@@ -1,6 +1,21 @@
 public class F04_PrePost {
     public static void main(String[] args) {
+int a=20;
+// a nın değerini oluşturduğunuz bir b varaiblesine atayın
+        //sonra a yı arttırın;
 
+        int b=a++;
+
+        System.out.println("11. satırda a: " +a+ ", b : "+b);// a=21 b=20
+
+        int g=a+1 ; //22
+        System.out.println("g : "+g);
+
+        int c=20;
+        // once c yı 1 artırın
+        // sonra c nin değerini oluşturduğunuz bir d varaiblesine atayın
+        int d=++c;
+        System.out.println("10. satırda c:"+c+" , d : "+ d);
 
 
 
