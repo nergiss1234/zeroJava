cf39fef053e57929fc6d209947b7b1bf42c389ad
Nergis <nergis.melahat@gmail.com>
2025-10-22 19:41:49 +0300

    2006

--- src/B03_RaperVeMatematikselİşlemler/F05_Concatenation.java
+++ src/B03_RaperVeMatematikselİşlemler/F05_Concatenation.java
@@ -9,6 +9,18 @@ public class F05_Concatenation {
         String s3="kolay";
         String s4="";
 
+        int a=3;
+        int b=4;
+        int c =a*b;
+        int d=a+b;
+        System.out.println(c+s2+s1+s2+s3);//12 Java kolay
+        System.out.println(d+s2+s1+s2+s3);//7 Java kolay
+        System.out.println(a+s4+b+s1+s2+s3);//34 Java kolay
+        System.out.println(s1+c+s3);//Java12kolay
+        System.out.println(s1+a+s4+b+s3);//Java34kolay
+        System.out.println(s1+d+s3);//Java7kolay
+
+
 
 
 
---
+++ src/B03_RaperVeMatematikselİşlemler/F06_Eşitlik.java
@@ -0,0 +1,46 @@
+package B03_RaperVeMatematikselİşlemler;
+
+public class F06_Eşitlik {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
