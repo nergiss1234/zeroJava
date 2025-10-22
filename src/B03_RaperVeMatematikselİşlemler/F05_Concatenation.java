d970efbf62a64f6a60d1a165788b2c1e5bf01bd3
Nergis <nergis.melahat@gmail.com>
2025-10-22 19:24:40 +0300

    1924

---
+++ src/B03_RaperVeMatematikselİşlemler/F05_Concatenation.java
@@ -0,0 +1,58 @@
+package B03_RaperVeMatematikselİşlemler;
+
+public class F05_Concatenation {
+    public static void main(String[] args) {
+
+        // Sadece verilen varaible'leri kullanarak String'leri elde edelim.
+        String s1="Java";
+        String s2=" ";
+        String s3="kolay";
+        String s4="";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
