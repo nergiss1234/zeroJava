25475346d9716304f6e6cce28e799be2cf6c2c97
Nergis <nergis.melahat@gmail.com>
2025-10-26 00:42:33 +0300

    261025

--- src/D05_IfStatements/F08_IfStatement.java
+++ src/D05_IfStatements/F08_IfStatement.java
@@ -2,7 +2,15 @@ package D05_IfStatements;
 
 public class F08_IfStatement {
     public static void main(String[] args) {
+// burdan devam edeceğiz....
 
+        boolean c=true;
+        if (c){
+
+
+            System.out.println(c);
+
+        }
 
 
 
