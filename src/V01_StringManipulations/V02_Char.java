30b605450df78828796be6f71bc84babf0ea5524
Nergis <nergis.melahat@gmail.com>
2025-11-19 20:00:22 +0300

    vo2

---
+++ src/V01_StringManipulations/V02_Char.java
@@ -0,0 +1,71 @@
+package V01_StringManipulations;
+
+import java.util.Locale;
+
+public class V02_Char {
+
+
+    public static void main(String[] args) {
+        String cümle="bağ bağcınındır";
+        cümle =cümle.toUpperCase();//"BAĞ BAĞCININDIR"
+
+        System.out.println(cümle.toLowerCase(Locale.forLanguageTag("Tr")));//bağ bağcınındır
+        //stringin ilk harfini yazdırın
+        String isim="Ramazan";
+        System.out.println(isim.charAt(0));//R
+        //isimdeki beşinci harfi yazdırın
+        System.out.println(isim.charAt(4));//Z
+        //son harfi yazdırın
+        System.out.println(isim.length());//7
+        System.out.println(isim.charAt(isim.length()-1));//n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
