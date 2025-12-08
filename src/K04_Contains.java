5a6e369a685cee8ffb2f2833eb909bd4691eb180
Nergis <nergis.melahat@gmail.com>
2025-12-08 19:40:40 +0300

    k05

---
+++ src/K04_Contains.java
@@ -0,0 +1,69 @@
+public class K04_Contains {
+    public static void main(String[] args) {
+        //contains methodu char kabul etmez...
+
+        String str=" hayatin her aşamasında değişime ayak uyduranlar hayatta kalırlar";
+
+        System.out.println(str.contains("a"));//true
+        System.out.println(str.contains("c"));//false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/K05_SubString.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class K05_SubString {
+    public static void main(String[] args) {
+        // kullanicidan bir metin isteyin
+// metin ev içeriyorsa "Ev gibisi var mi"
+// metin iş içeriyorsA "Çalışmak ne güzel"
+// metin hem ev hem iş içeriyorsa "Evden çalışmak gibisi yok"
+// metin bu iki kelimeyi de içermiyorsa "Çok çalışmak lazim, çoook" yazdırın
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("lütfen bir metin giriniz .....");
+        String girilenMetin=scanner.nextLine();
+        if(girilenMetin.contains("ev")&&girilenMetin.contains("iş")){
+            System.out.println("çok çalışmak lazım ,çoook");
+
+        }
+        if (girilenMetin.contains("ev")){
+            System.out.println("ev gibisi var mı");
+        }
+        if (girilenMetin.contains("iş")){
+            System.out.println("evden çalışmak ne güzel");
+        }
+        if(girilenMetin.contains("iş ")&&girilenMetin.contains("ev")){
+            System.out.println("evden çalışmak gibisi yok");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
