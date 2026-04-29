da352fb1e9be381e107b000537a9b2bf5a307dad
Nergis <nergis.melahat@gmail.com>
2026-04-29 20:13:03 +0300

    29.04.2026

---
+++ src/P01_NestedForLoop/P05_Tekrar.java
@@ -0,0 +1,65 @@
+package P01_NestedForLoop;
+
+public class P05_Tekrar {
+    public static void main(String[] args) {
+        /*
+    *
+    * *
+    * * *
+    * * * *
+        
+        
+         */
+        for (int i = 0; i <4 ; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                System.out.print("*");
+                
+            }
+
+            
+            
+        }       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
