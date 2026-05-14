1b8ef858f0c0104148869ed5b6c6f5911f45bdac
Nergis <nergis.melahat@gmail.com>
2026-05-14 19:31:55 +0300

    14.05.2026

---
+++ src/K016_WhileLoop/K017_WhileLoop.java
@@ -0,0 +1,69 @@
+package K016_WhileLoop;
+
+public class K017_WhileLoop {
+    public static void main(String[] args) {
+    //eğer başlangıç ve bitiş değeri biliniyorsa
+        //veya bir işlemi kaç defa tekrarlayacağımızı
+        //biliyorsak for lop avantajlıdır çünkü while
+        //başlangıç değeri ve bitiş değeri ve artiş/azalış
+        //miktarını bize olarak vermez
+
+
+        //1 den 10 a kadar (1 ve 10dahil) tüm sayıları
+        //toplayınız....
+
+
+sayılarArasınıTopla(2,3);
+sayılarArasınıTopla(1,55);
+//aynısını while loop ile yapalım
+        int toplam=0;
+int sayı=1;
+while (sayı<=10){
+    toplam+=sayı;
+    sayı++;
+
+}
+        System.out.println("while loop sonuç"+toplam);
+//20 den 30 a kadar olan çift sayıların toplamını bulunuz
+        for (int i = 20; i <=30 ; i++) {
+
+        }
+
+
+
+
+
+    }
+
+ public static void sayılarArasınıTopla(int sayı1,int sayı2){
+
+   int toplam=0;
+     for (int i = sayı1; i <=sayı2; i++) {
+         toplam+=i;
+
+
+     }
+     System.out.println(sayı1+"den"+sayı2+"ye kadar sayıları topla"+toplam);
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+}
