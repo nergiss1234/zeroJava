9d84fa1716a308beffaba74721e077eca77af086
Nergis <nergis.melahat@gmail.com>
2026-05-17 19:33:38 +0300

    17.05.2026

---
+++ src/K016_WhileLoop/K020_EnUzunKelime.java
@@ -0,0 +1,41 @@
+package K016_WhileLoop;
+
+public class K020_EnUzunKelime {
+    public static void main(String[] args) {
+        //kullanıcıdan istediği kadar isim girmesini isteyin
+        //kullanıcı işlemi bitirmek için q ya basmalıdır(q veya Q olabilir)
+        //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/K19_WhileLoopUnAvantajlıOlduğuDurum.java
+++ src/K19_WhileLoopUnAvantajlıOlduğuDurum.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class K19_WhileLoopUnAvantajlıOlduğuDurum {
     public static void main(String[] args) {
 
@@ -5,7 +7,25 @@ public class K19_WhileLoopUnAvantajlıOlduğuDurum {
         //girilen sayılar pozitif ise sayıyı toplama ekleyin
         //girilen sayı negatif ise uyarı verin ve sayıyı işleme almayın
         //girilen sayı sıfır olduğunda işlemi bitirin
+        // ve kaç tane pozitif tam sayı girildiğini ve toplamlarını yazdırın
+        Scanner scanner=new Scanner(System.in);
+        int sayı1=10;
+        //sıfırdan farklı bir sayı veriyoruz çünkü
+        // 0 bir condition yani şart
+        int  toplam=0;
+        int sayaç =0;
+        while (sayı1!=0){
+            System.out.println("lütfen pozitif bir sayı girin");
+            sayı1=scanner.nextInt();
+            if (sayı1>0){
+                toplam+=sayı1;
+                sayaç++;
+            } else if (sayı1<0) {
+                System.out.println("girilen sayı negatif sayı olamaz");
 
+            }
+        }
+        System.out.println("girilen"+sayaç+"adet pozitif tam sayının toplamı "+toplam);
 
 
 
